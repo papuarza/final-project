d5a3ef973d5b6d1d076a68f9fe35cc199a2feb7e
Papu Arza <papuarza@MacBook-Pro-de-Papu.local>
2017-03-22 15:28:40 +0100

    hola

--- client/src/app/session.service.ts
+++ client/src/app/session.service.ts
@@ -3,8 +3,9 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import { Observable } from 'rxjs/Rx';
+import { environment } from '../environments/environment';
 
-const baseURL = "http://localhost:3000/"
+const baseURL = environment.apiUrl
 
 @Injectable()
 export class SessionService {
